faffb107a967d4c1f8cdd025d29cf593e8132a9e
ericeberg3 <139914913+ericeberg3@users.noreply.github.com>
2026-05-15 11:36:06 -0700

    Adding timing things to try to speed things up

--- compute_obj.py
+++ compute_obj.py
@@ -81,4 +81,4 @@ def compute_grad_k0(fwd, adj, M):
 def compute_grad_k12(fwd, adj, M):
     """dJ/dk12 = int [ (lam2 - lam1)*(u1 - u2) ] dt"""
     del M  # k12 gradient has no explicit M dependence; M kept for API consistency
-    return np.trapz((adj['lam2'] - adj['lam1']) * (fwd['u1'] - fwd['u2']), fwd['t'])
+    return np.trapz(-(adj['lam2'] - adj['lam1']) * (fwd['u1'] - fwd['u2']), fwd['t'])
